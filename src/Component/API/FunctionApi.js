b804b1c6a91de501cdface239b26232514319c1d
VIJAYARAJ-M <vijayarajm.2016@gmail.com>
2023-04-30 18:04:43 +0530

    Final commit

--- src/Component/API/FunctionApi.js
+++ src/Component/API/FunctionApi.js
@@ -5,18 +5,34 @@ export default function FunctionApi(){
     const [data,setdata]=useState([]);
     console.log("d",data);
 
+    const [person, setPerson] = useState(null);
+    console.log("Person",person);
+
    
     const get =()=>{
-        fetch("https://jsonplaceholder.typicode.com/users/")
+        fetch("https://jsonplaceholder.typicode.com/users")
         .then(res=>res.json())
         .then(response=>setdata(response))
     }
+    
 
+    // const Handle = (item) =>{
+    //   console.log("Name",item);
+    //   fetch(`https://jsonplaceholder.typicode.com/users/${item.id}`)
+    //   .then(Res=>Res.json())
+    //   .then(result=>setPerson(result))
+    // }
+
+     const Delete = (id) =>{
+       const newdata =data.filter((data)=>data.id !== id)
+       setdata(newdata)
+      
+     }
     return(
         <div>
            <h2>Hiii</h2>
            {data.map((item)=>(
-                   <h4 key={item.id}>{item.name}</h4>
+                   <h4 key={item.id}>{item.id}.{item.username} <button onClick={()=>Handle(item)}>{item.username}</button></h4>
                      ///Array of object mapping
            ))} 
 
